84460751a4981ba22e0e27253cbade2bd5e30d6f
Grey <grey@localhost>
2026-09-12 12:14:26 +0530

    feat: add unique violation detection helper

---
+++ backend/internal/store/pg.go
@@ -0,0 +1,13 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+// isUniqueViolation reports whether err is a PostgreSQL unique-violation error.
+func isUniqueViolation(err error) bool {
+	var pgErr *pgconn.PgError
+	return errors.As(err, &pgErr) && pgErr.Code == "23505"
+}
